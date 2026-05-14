e17b0d313dd6c56bfc0147f1abab8c644c595968
AdeshKumar07 <adeshak0707@gmail.com>
2026-05-14 17:46:47 +0530

    commit

--- ROLLNUMBER/priority-notification-app/vite.config.js
+++ ROLLNUMBER/priority-notification-app/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      allow: [fileURLToPath(new URL("../", import.meta.url))],
+    },
+  },
 });
